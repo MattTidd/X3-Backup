a5ed5ba85a19643b952a888eb3d33db8bd8f9552
MattTidd <mtidd2@unb.ca>
2024-08-01 09:57:15 -0300

    added humble docker file

---
+++ humble.sh
@@ -0,0 +1,24 @@
+xhost +local:
+
+docker run -it \
+--net=host \
+--env="DISPLAY" \
+--env="QT_X11_NO_MITSHM=1" \
+--volume /tmp/.X11-unix:/tmp/.X11-unix:rw \
+-v /dev/bus/usb/001/007:/dev/bus/usb/001/007 \
+-v /dev/bus/usb/001/009:/dev/bus/usb/001/009 \
+--device=/dev/astradepth \
+--device=/dev/astrauvc \
+--device=/dev/video0 \
+--device=/dev/myserial \
+--device=/dev/rplidar \
+--device=/dev/ttyUSB0 \
+--device=/dev/ttyUSB1 \
+-p 9090:9090 \
+-p 8888:8888 \
+matttidd/humble:latest /bin/bash
+
+#-v /home/jetson/temp:/root/yahboomcar_ros2_ws/temp \
+#-v /home/jetson/rosboard:/root/rosboard \
+#-v /home/jetson/maps:/root/maps \
+
